267b3008a4616490cb4dfe9cc69f4ec5befa34e4
Nilav Patel <nilavpatel1992@gmail.com>
2020-08-30 15:19:24 +0530

    Refactor gaurd class and add ability to generate custom gaurd clause

--- Guard-Pattern.Test/GuardTest.cs
+++ Guard-Pattern.Test/GuardTest.cs
@@ -8,327 +8,327 @@ namespace Guard_Pattern.Test
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void Test_String_AgainstNull()
+        public void Test_String_Against_Null()
         {
             string name = null;
-            Guard.AgainstNull(name, "Name");
+            Guard.Against.Null(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void Test_DateTime_AgainstNull()
+        public void Test_DateTime_Against_Null()
         {
             DateTime? startDate = null;
-            Guard.AgainstNull(startDate, "StartDate");
+            Guard.Against.Null(startDate, "StartDate");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void Test_Int_AgainstNull()
+        public void Test_Int_Against_Null()
         {
             int? money = null;
-            Guard.AgainstNull(money, "Money");
+            Guard.Against.Null(money, "Money");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void Test_String_AgainstNullOrEmpty()
+        public void Test_String_Against_NullOrEmpty()
         {
             string name = "";
-            Guard.AgainstNullOrEmpty(name, "Name");
+            Guard.Against.NullOrEmpty(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void Test_String_AgainstNullOrWhiteSpace()
+        public void Test_String_Against_NullOrWhiteSpace()
         {
             string name = " ";
-            Guard.AgainstNullOrWhiteSpace(name, "Name");
+            Guard.Against.NullOrWhiteSpace(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_LeadingSpace_AgainstLeadingAndTailingSpace()
+        public void Test_String_LeadingSpace_Against_LeadingAndTailingSpace()
         {
             string name = " nilav";
-            Guard.AgainstLeadingAndTailingSpace(name, "Name");
+            Guard.Against.LeadingAndTailingSpace(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_TailingSpace_AgainstLeadingAndTailingSpace()
+        public void Test_String_TailingSpace_Against_LeadingAndTailingSpace()
         {
             string name = "nilav ";
-            Guard.AgainstLeadingAndTailingSpace(name, "Name");
+            Guard.Against.LeadingAndTailingSpace(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstSpace()
+        public void Test_String_Against_Space()
         {
             string name = "nilav patel";
-            Guard.AgainstSpace(name, "Name");
+            Guard.Against.Space(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstMinimumLength()
+        public void Test_String_Against_MinimumLength()
         {
             string name = "nilav";
-            Guard.AgainstMinimumLength(name, "Name", 10);
+            Guard.Against.MinimumLength(name, "Name", 10);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstMaximumLength()
+        public void Test_String_Against_MaximumLength()
         {
             string name = "nilav patel";
-            Guard.AgainstMaximumLength(name, "Name", 5);
+            Guard.Against.MaximumLength(name, "Name", 5);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstSpecialCharacters()
+        public void Test_String_Against_SpecialCharacters()
         {
             string name = "nilav@patel";
-            Guard.AgainstSpecialCharacters(name, "Name");
+            Guard.Against.SpecialCharacters(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstSpecialCharacters2()
+        public void Test_String_Against_SpecialCharacters2()
         {
             string name = "nilav.patel";
-            Guard.AgainstSpecialCharacters(name, "Name");
+            Guard.Against.SpecialCharacters(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstSpecialCharacters3()
+        public void Test_String_Against_SpecialCharacters3()
         {
             string name = "nilav/patel";
-            Guard.AgainstSpecialCharacters(name, "Name");
+            Guard.Against.SpecialCharacters(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstSpecialCharacters4()
+        public void Test_String_Against_SpecialCharacters4()
         {
             string name = "nilav%patel";
-            Guard.AgainstSpecialCharacters(name, "Name");
+            Guard.Against.SpecialCharacters(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstSpecialCharacters5()
+        public void Test_String_Against_SpecialCharacters5()
         {
             string name = "nilav$patel";
-            Guard.AgainstSpecialCharacters(name, "Name");
+            Guard.Against.SpecialCharacters(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstDigits()
+        public void Test_String_Against_Digits()
         {
             string name = "nilav patel 123";
-            Guard.AgainstDigits(name, "Name");
+            Guard.Against.Digits(name, "Name");
         }
 
         [TestMethod]
-        public void Test_String_AgainstDigits_Valid()
+        public void Test_String_Against_Digits_Valid()
         {
             string name = "nilav patel";
-            Guard.AgainstDigits(name, "Name");
+            Guard.Against.Digits(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstAlphabet()
+        public void Test_String_Against_Alphabet()
         {
             string name = "nilav patel";
-            Guard.AgainstAlphabet(name, "Name");
+            Guard.Against.Alphabet(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstLowerCase()
+        public void Test_String_Against_LowerCase()
         {
             string name = "nilav patel";
-            Guard.AgainstLowerCase(name, "Name");
+            Guard.Against.LowerCase(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_String_AgainstUpperCase()
+        public void Test_String_Against_UpperCase()
         {
             string name = "NILAV PATEL";
-            Guard.AgainstUpperCase(name, "Name");
+            Guard.Against.UpperCase(name, "Name");
         }
 
         [TestMethod]
-        public void Test_String_AgainstUpperCase_Valid()
+        public void Test_String_Against_UpperCase_Valid()
         {
             string name = "nilav patel 123";
-            Guard.AgainstUpperCase(name, "Name");
+            Guard.Against.UpperCase(name, "Name");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Number_AgainstNumberLessThan()
+        public void Test_Number_Against_NumberLessThan()
         {
             int money = 500;
-            Guard.AgainstNumberLessThan(money, "Money", 1000);
+            Guard.Against.NumberLessThan(money, "Money", 1000);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Number_AgainstNumberGreaterThan()
+        public void Test_Number_Against_NumberGreaterThan()
         {
             int money = 1000;
-            Guard.AgainstNumberGreaterThan(money, "Money", 500);
+            Guard.Against.NumberGreaterThan(money, "Money", 500);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Number_AgainstNumberLessThanOrEqual()
+        public void Test_Number_Against_NumberLessThanOrEqual()
         {
             int money = 500;
-            Guard.AgainstNumberLessThanOrEqual(money, "Money", 500);
+            Guard.Against.NumberLessThanOrEqual(money, "Money", 500);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Number_AgainstNumberGreaterThanOrEqual()
+        public void Test_Number_Against_NumberGreaterThanOrEqual()
         {
             int money = 1000;
-            Guard.AgainstNumberGreaterThanOrEqual(money, "Money", 1000);
+            Guard.Against.NumberGreaterThanOrEqual(money, "Money", 1000);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Number_AgainstNumberZero()
+        public void Test_Number_Against_NumberZero()
         {
             int money = 0;
-            Guard.AgainstNumberZero(money, "Money");
+            Guard.Against.NumberZero(money, "Money");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Number_AgainstNumberNegativeOrZero()
+        public void Test_Number_Against_NumberNegativeOrZero()
         {
             int money = -1000;
-            Guard.AgainstNumberNegativeOrZero(money, "Money");
+            Guard.Against.NumberNegativeOrZero(money, "Money");
         }
 
         [TestMethod]
-        public void Test_Number_AgainstNumberNegativeOrZero_Valid()
+        public void Test_Number_Against_NumberNegativeOrZero_Valid()
         {
             int money = 1000;
-            Guard.AgainstNumberNegativeOrZero(money, "Money");
+            Guard.Against.NumberNegativeOrZero(money, "Money");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Number_AgainstNumberNegative()
+        public void Test_Number_Against_NumberNegative()
         {
             int money = -1000;
-            Guard.AgainstNumberNegative(money, "Money");
+            Guard.Against.NumberNegative(money, "Money");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Number_AgainstNumberOutOfRange()
+        public void Test_Number_Against_NumberOutOfRange()
         {
             int money = 1000;
-            Guard.AgainstNumberOutOfRange(money, "Money", 500, 600);
+            Guard.Against.NumberOutOfRange(money, "Money", 500, 600);
         }
 
         [TestMethod]
-        public void Test_Number_AgainstNumberOutOfRange_Valid()
+        public void Test_Number_Against_NumberOutOfRange_Valid()
         {
             int money = 1000;
-            Guard.AgainstNumberOutOfRange(money, "Money", 500, 2000);
+            Guard.Against.NumberOutOfRange(money, "Money", 500, 2000);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_DateTime_AgainstDateTimeGreaterThan()
+        public void Test_DateTime_Against_DateTimeGreaterThan()
         {
             DateTime startDate = DateTime.Now;
-            Guard.AgainstDateTimeGreaterThan(startDate, "StartDate", DateTime.Now.AddDays(-1));
+            Guard.Against.DateTimeGreaterThan(startDate, "StartDate", DateTime.Now.AddDays(-1));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_DateTime_AgainstDateTimeLessThanOrEqual()
+        public void Test_DateTime_Against_DateTimeLessThanOrEqual()
         {
             DateTime startDate = DateTime.Now;
-            Guard.AgainstDateTimeLessThanOrEqual(startDate, "StartDate", DateTime.Now.AddDays(1));
+            Guard.Against.DateTimeLessThanOrEqual(startDate, "StartDate", DateTime.Now.AddDays(1));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_URL_AgainstInValidURL()
+        public void Test_URL_Against_InValidURL()
         {
-            Guard.AgainstInValidURL("h://nilavpatel.github.io/", "URL");
+            Guard.Against.InValidURL("h://nilavpatel.github.io/", "URL");
         }
 
         [TestMethod]
-        public void Test_URL_AgainstInValidURL_Valid()
+        public void Test_URL_Against_InValidURL_Valid()
         {
-            Guard.AgainstInValidURL("https://nilavpatel.github.io", "URL");
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void Test_EmailId_AgainstInValidEmailId()
-        {
-            Guard.AgainstInValidEmailId("nilavpatel@1992@gmail.com", "EmailId");
-        }
-
-        [TestMethod]
-        public void Test_EmailId_AgainstInValidEmailId_Valid()
-        {
-            Guard.AgainstInValidEmailId("nilavpatel1992@gmail.com", "EmailId");
+            Guard.Against.InValidURL("https://nilavpatel.github.io", "URL");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Guid_AgainstInValidGuid()
+        public void Test_EmailId_Against_InValidEmailId()
         {
-            Guard.AgainstInValidGuid("AS52-58695-632563-ASHSJN", "Id");
+            Guard.Against.InValidEmailId("nilavpatel@1992@gmail.com", "EmailId");
         }
 
         [TestMethod]
-        public void Test_Guid_AgainstInValidGuid_Valid()
+        public void Test_EmailId_Against_InValidEmailId_Valid()
         {
-            Guard.AgainstInValidGuid(Guid.NewGuid().ToString(), "Id");
-        }
-
-        [TestMethod]
-        public void Test_Guid_AgainstFalse_Valid()
-        {
-            Guard.AgainstFalse(true, "Active");
-        }
-
-        [TestMethod]
-        public void Test_Guid_AgainstTrue_Valid()
-        {
-            Guard.AgainstTrue(false, "Active");
+            Guard.Against.InValidEmailId("nilavpatel1992@gmail.com", "EmailId");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Guid_AgainstFalse_InValid()
+        public void Test_Guid_Against_InValidGuid()
         {
-            Guard.AgainstFalse(false, "Active");
+            Guard.Against.InValidGuid("AS52-58695-632563-ASHSJN", "Id");
+        }
+
+        [TestMethod]
+        public void Test_Guid_Against_InValidGuid_Valid()
+        {
+            Guard.Against.InValidGuid(Guid.NewGuid().ToString(), "Id");
+        }
+
+        [TestMethod]
+        public void Test_Guid_Against_False_Valid()
+        {
+            Guard.Against.False(true, "Active");
+        }
+
+        [TestMethod]
+        public void Test_Guid_Against_True_Valid()
+        {
+            Guard.Against.True(false, "Active");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Test_Guid_AgainstTrue_InValid()
+        public void Test_Guid_Against_False_InValid()
         {
-            Guard.AgainstTrue(true, "Active");
+            Guard.Against.False(false, "Active");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_Guid_Against_True_InValid()
+        {
+            Guard.Against.True(true, "Active");
         }
     }
 }
---
+++ Guard-Pattern/IGuardClause.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Guard_Pattern
+{
+    public interface IGuardClause
+    {
+    }
+}
